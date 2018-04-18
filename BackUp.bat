ba1d09e87f3e164b3b8b327133741c62c6966b9c
vsonka <sonkarvikas0786@gmail.com>
2018-04-18 19:43:32 +0530

    Update BackUp.bat

--- BackUp.bat
+++ BackUp.bat
@@ -3,7 +3,7 @@ SET date="%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,
 md "D:\BackUP\CICDDEMO\%date%"
 xcopy "C:\inetpub\wwwroot\CICDDEMO" "D:\BackUP\CICDDEMO\%date%\" /e /s
 @echo Backup completed.
-c:\windows\system32\inetsrv\appcmd.exe set site /site.name:"CICDDEMOBackUp" /application[path='/'].virtualDirectory[path='/'].physicalPath:"D:\BackUP\CICDDEMO\%date%\"
+c:\windows\system32\inetsrv\appcmd.exe set site /site.name:"CICDDEMOBackUp" /application[path='/'].virtualDirectory[path='/'].physicalPath:"D:\BackUP\CICDDEMO\%date%"
 pause
 
 
