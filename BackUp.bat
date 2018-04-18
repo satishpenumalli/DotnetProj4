e3dda01546247a39e701e23ca6076473b6d5fe1e
vsonka <sonkarvikas0786@gmail.com>
2018-04-18 19:44:50 +0530

    Update BackUp.bat

--- BackUp.bat
+++ BackUp.bat
@@ -1,5 +1,5 @@
 @echo Backup started...
-SET date="%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%"
+SET date=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
 md "D:\BackUP\CICDDEMO\%date%"
 xcopy "C:\inetpub\wwwroot\CICDDEMO" "D:\BackUP\CICDDEMO\%date%\" /e /s
 @echo Backup completed.
